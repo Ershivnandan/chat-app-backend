5d130e7cdc352ea3d5dcafce452e2c57a09cbe5b
ErShivnandan <ec1912@global.org.in>
2024-11-22 01:15:36 +0530

    passport updated

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,35 +2,38 @@ import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20'; 
 import User from '../models/user.modal.js';
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID, 
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-    callbackURL: `${process.env.AZURE_API}/api/users/auth/google/callback`,
-    passReqToCallback   : true,
-}, async (accessToken, refreshToken, profile, done) => {
-    try {
-        
-        const existingUser = await User.findOne({ googleId: profile.id });
-
-        if (existingUser) {
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: `${process.env.AZURE_API}/api/users/auth/google/callback`,
+        passReqToCallback: true,
+      },
+      async (req, accessToken, refreshToken, profile, done) => {
+        try {
+          const existingUser = await User.findOne({ googleId: profile.id });
+  
+          if (existingUser) {
             return done(null, existingUser);
-        }
-
-       
-        const newUser = await new User({
+          }
+  
+          const newUser = await new User({
             username: profile.displayName,
             googleId: profile.id,
             email: profile.emails[0].value,
-            password: null, 
-        }).save();
-
-        done(null, newUser);  
-    } catch (error) {
-        console.error('Error during Google login:', error);  
-        done(error, null); 
-    }
-}));
-
+            password: null,
+          }).save();
+  
+          done(null, newUser);
+        } catch (error) {
+          console.error("Error during Google login:", error);
+          done(error, null);
+        }
+      }
+    )
+  );
+  
 
 passport.serializeUser((user, done) => {
     done(null, user.id);  
