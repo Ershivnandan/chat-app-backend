8fde5b1a59a4b13723f1ef64d0a317cfb4105806
ErShivnandan <ec1912@global.org.in>
2024-11-21 22:19:15 +0530

    passport update

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,18 +1,22 @@
 import passport from 'passport';
-import GoogleStrategy from 'passport-google-oauth20';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20'; 
 import User from '../models/user.modal.js';
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/users/auth/google/callback',
+    clientID: process.env.GOOGLE_CLIENT_ID, 
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+    callbackURL: `${process.env.AZURE_API}/api/users/auth/google/callback`,
 }, async (accessToken, refreshToken, profile, done) => {
     try {
+        
         const existingUser = await User.findOne({ googleId: profile.id });
+
         if (existingUser) {
+        
             return done(null, existingUser);
         }
 
+       
         const newUser = await new User({
             username: profile.displayName,
             googleId: profile.id,
@@ -20,18 +24,24 @@ passport.use(new GoogleStrategy({
             password: null, 
         }).save();
 
-        done(null, newUser);
+        done(null, newUser);  
     } catch (error) {
-        done(error, null);
+        console.error('Error during Google login:', error);  
+        done(error, null); 
     }
 }));
 
+
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user.id);  
 });
 
+
 passport.deserializeUser((id, done) => {
+  
     User.findById(id).then(user => {
-        done(null, user);
+        done(null, user);  
+    }).catch(err => {
+        done(err, null);  
     });
 });
